b863589a5be8884289d1651a41c0e1c8e4d40e39
雪遥 张 <zhangxueyao1998@outlook.com>
2019-07-10 16:46:26 +0800

    start ffmpeg videos

--- GetFeature/resource.h
+++ GetFeature/resource.h
@@ -8,24 +8,22 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_GETFEATURE_DIALOG           102
 #define IDR_MAINFRAME                   128
-#define IDD_DIALOG1_JIAOHU              130
-#define IDD_DIALOG2_PILIANG             131
-#define IDD_DIALOG3_TEZHENG             132
-#define IDD_DIALOG4_CHACHONG            133
+#define IDD_DIALOG1_JIAOHU              146
+#define IDD_DIALOG2_PILIANG             147
+#define IDD_DIALOG3_TEZHENG             148
+#define IDD_DIALOG4_CHACHONG            149
 #define IDC_TAB1                        1000
 #define IDC_BUTTON1                     1001
-#define IDC_BUTTON_OPEN                 1001
-#define IDC_EDIT_FEATURE                1002
-#define IDC_VIEW                        1003
-#define IDC_SLIDER1                     1004
+#define IDC_EDIT_FEATURE                1034
+#define IDC_BUTTON_OPEN                 1047
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_RESOURCE_VALUE        150
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
